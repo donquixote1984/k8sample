75e0af21095d7a351769a6b561b4165e7a41d35a
donquixote1984 <donquixote1984@163.com>
2022-03-27 10:42:29 +0800

    init

--- app/my-app/pages/index.js
+++ app/my-app/pages/index.js
@@ -1,9 +1,36 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {useState, useEffect} from 'react'
 
+const token = process.env.API_TOKEN;
+
+console.log(token);
 export default function Home() {
+  const [searchTxt, setSearchTxt] = useState("");
+  const [list, setList]= useState({});
+  
+
+  useEffect(() => {
+    
+  }, [])
+
+  const onKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      doSearch();
+    }
+  }
+  const doSearch = () => {
+    const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchTxt}&key=AIzaSyBOaQR9WxdSm6AOMbfSn-aCZeDpMH0T_Bw`;
+    fetch(url).then(r => r.json()).then(data => {
+      console.log(data);
+      setList(data);
+  })
+  }
   return (
+
+
+
     <div className={styles.container}>
       <Head>
         <title>Create Next App V2</title>
@@ -11,59 +38,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1>Welcome to Next Sample</h1>
+        <h4 className={styles.title}>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <div>
+            <span style={{fontSize: 20, marginRight: 10}}>Search</span>
+            <input type='text' value={searchTxt} onChange={(e) => setSearchTxt(e.target.value)} style={{padding: 10, width: 600}} onKeyDown={onKeyDown}/>
+          </div>
+        </h4>
+        <ul>
+         {
+          list.items && list.items.map(item => 
+            <li key={item.etag}>
+            <div>
+              <div>{item.snippet.title}</div>
+              <div>
+                {
+                    <img src={item.snippet.thumbnails.default.url} width={item.snippet.thumbnails.default.width} height={item.snippet.thumbnails.default.height}/>
+                }
+              </div>
+            </div>
+            </li>
+          )
+        }
+        </ul>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        
     </div>
   )
 }
